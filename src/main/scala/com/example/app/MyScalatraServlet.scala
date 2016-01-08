c0ae843c9e893788b78a6eb2b91423b7e44dded7
wakayama <y-wakayama@mcc-osaka.co.jp>
2016-01-08 20:27:14 +0900

    bayes modelのロード、classifyを追加。Json APIが実装できそうな気配

--- src/main/scala/com/example/app/MyScalatraServlet.scala
+++ src/main/scala/com/example/app/MyScalatraServlet.scala
@@ -2,14 +2,32 @@ package com.example.app
 
 import org.scalatra._
 import scalate.ScalateSupport
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+import org.apache.spark.mllib.classification.{NaiveBayes, NaiveBayesModel}
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.feature.{HashingTF, IDF}
 
 class MyScalatraServlet extends MyScalatraWebAppStack {
+    val conf = new SparkConf().setAppName("simple application").setMaster("local")
+    val sc = new SparkContext(conf)
+    val labels = sc.objectFile("model/labels.txt")
+    val texts = sc.objectFile("model/texts.txt")
+    val htf = new HashingTF(1000)
+    val tf = htf.transform(texts)
+    val model = NaiveBayesModel.load(sc, "model")
+    val words = List("武将"," 大名", "天下人", "関白", "太閤")
+    val test_tf = htf.transform(words)
+    val test = model.predict(test_tf)
 
   get("/") {
     <html>
       <body>
         <h1>Hello, world!</h1>
         Say <a href="hello-scalate">hello to Scalate</a>.
+         <p>{test}</p>
       </body>
     </html>
   }
