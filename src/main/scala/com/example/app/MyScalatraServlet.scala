a6ebe9a99970c060503a15e4c20536039f419de6
wakayama <y-wakayama@mcc-osaka.co.jp>
2016-01-11 13:27:53 +0900

    json4s試したけどダメだった...

--- src/main/scala/com/example/app/MyScalatraServlet.scala
+++ src/main/scala/com/example/app/MyScalatraServlet.scala
@@ -11,25 +11,23 @@ import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.mllib.feature.{HashingTF, IDF}
 
 class MyScalatraServlet extends MyScalatraWebAppStack {
-    val conf = new SparkConf().setAppName("simple application").setMaster("local")
-    val sc = new SparkContext(conf)
-    val labels = sc.objectFile("model/labels.txt")
-    val texts = sc.objectFile("model/texts.txt")
-    val htf = new HashingTF(1000)
-    val tf = htf.transform(texts)
-    val model = NaiveBayesModel.load(sc, "model")
-    val words = List("武将"," 大名", "天下人", "関白", "太閤")
-    val test_tf = htf.transform(words)
-    val test = model.predict(test_tf)
+  val conf = new SparkConf().setAppName("simple application").setMaster("local")
+  val sc = new SparkContext(conf)
+  val labels = sc.objectFile("model/labels.txt")
+  val texts = sc.objectFile("model/texts.txt")
+  val htf = new HashingTF(1000)
+  val tf = htf.transform(texts)
+  val model = NaiveBayesModel.load(sc, "model")
+  val words = List("武将"," 大名", "天下人", "関白", "太閤")
+  val test_tf = htf.transform(words)
+  val test = model.predict(test_tf)
 
   get("/") {
     <html>
       <body>
-        <h1>Hello, world!</h1>
-        Say <a href="hello-scalate">hello to Scalate</a>.
-         <p>{test}</p>
+        <h1>Hello, World!</h1>
+        <p>{test}</p>
       </body>
     </html>
   }
-
 }
